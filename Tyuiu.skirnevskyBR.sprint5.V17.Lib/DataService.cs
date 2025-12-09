1f210bc830c4393868185c77734bf771bd0c564b
Богдан Скирневский <tyuiu.skirnevskybr@outlook.com>
2025-12-09 21:19:53 +0500

    Task0

---
+++ Tyuiu.skirnevskyBR.sprint5.V17.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.skirnevskyBR.sprint5.V17.Lib
+{
+    public class DataService : ISprint5Task0V17
+    {
+        public string SaveToFileTextData(int x)
+        {
+
+            double result = 2.4 * Math.Pow(x, 3) + 0.4 * Math.Pow(x, 2) - 1.4 * x + 4.1;
+
+            result = Math.Round(result, 3);
+
+            string path = Path.GetTempFileName();
+
+            File.WriteAllText(path, result.ToString());
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.skirnevskyBR.sprint5.V17.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.skirnevskyBR.sprint5.V17.Lib;
+namespace Tyuiu.skirnevskyBR.sprint5.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+
+            DataService ds = new DataService();
+            int x = 3;
+
+            double expected = 2.4 * Math.Pow(3, 3) + 0.4 * Math.Pow(3, 2) - 1.4 * 3 + 4.1;
+            expected = Math.Round(expected, 3); 
+
+            string path = ds.SaveToFileTextData(x);
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.IsTrue(fileExists, "Файл не был создан");
+
+            string fileContent = File.ReadAllText(path);
+            double actual = double.Parse(fileContent);
+
+            Assert.AreEqual(expected, actual, 0.001, "Значение в файле не соответствует ожидаемому");
+        }
+    }
+}
